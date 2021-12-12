f857a0e2f26b7cd5bc30855907de7de48bb98205
Kartik <9415kks@gmail.com>
2021-12-12 18:51:59 +0530

    Learn about redux toolkit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartRedux";
 import userReducer from "./userRedux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "@reduxjs/toolkit";
+import storage from "@reduxjs/toolkit";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export default configureStore({
   reducer: {
     cart: cartReducer,
     user: userReducer,
   },
-});
+}); 
