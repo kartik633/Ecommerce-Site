9c3556bd673b31db36d0b85ac548dc8cd6c41fef
Kartik <9415kks@gmail.com>
2021-12-30 10:00:27 +0530

    Added Changes

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:3000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWE1YmU4MWE5YmRhMTBiNTY0YTY0ZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzODc3MjA2NSwiZXhwIjoxNjM5MDMxMjY1fQ.zU6V7BG5wuFbXR7XdbtMLNlFn1pYvVdFVsv4M75-u2A";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).currentUser).accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
